123849951334d613a2a7ec440bd0cc750ef56b3a
alynn01 <127193049+alynn01@users.noreply.github.com>
2023-04-09 08:36:59 +0100

    login test update
    Added some edge cases to the login test and introduced a before each statement

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,10 +1,40 @@
 describe('Login', () => {
-  it('Test login with valid credentials that is the correct email and password combination', () => {
+  beforeEach(() => {
     cy.visit('https://www.saucedemo.com/');
     cy.get(`[class="login_logo"]`).contains('Swag Labs')
+  })
+
+  it('Test login with valid credentials that is the correct username and password combination', () => {
     cy.get(`input[data-test="username"]`).type('standard_user');
     cy.get(`input[data-test="password"]`).type('secret_sauce'); 
     cy.get(`input[data-test="login-button"]`).click();
     cy.get(`[class="app_logo"]`).contains('Swag Labs').should('be.visible')
   })
+
+  it('Test that the user is unable to log in with an invalid password but a valid username combination', () => {
+    cy.get(`input[data-test="username"]`).type('standard_user');
+    cy.get(`input[data-test="password"]`).type('secret_saue'); 
+    cy.get(`input[data-test="login-button"]`).click();
+    cy.get("h3").contains('Epic sadface: Username and password do not match any user in this service').should('be.visible')
+  })
+
+  it('Test that the user is unable to log in with a valid password but an invalid username combination', () => {
+    cy.get(`input[data-test="username"]`).type('standard_usr');
+    cy.get(`input[data-test="password"]`).type('secret_sauce'); 
+    cy.get(`input[data-test="login-button"]`).click();
+    cy.get("h3").contains('Epic sadface: Username and password do not match any user in this service').should('be.visible')
+  })
+
+  it('Test that the user is unable to log in with an empty username but a valid password field', () => {
+    cy.get(`input[data-test="password"]`).type('secret_sauce'); 
+    cy.get(`input[data-test="login-button"]`).click();
+    cy.get("h3").contains('Epic sadface: Username is required').should('be.visible')
+  })
+
+  it('Test that the user is unable to log in with an empty password', () => {
+    cy.get(`input[data-test="username"]`).type('standard_user');
+    cy.get(`input[data-test="login-button"]`).click();
+    cy.get("h3").contains('Epic sadface: Password is required').should('be.visible')
+  })
+
 })
